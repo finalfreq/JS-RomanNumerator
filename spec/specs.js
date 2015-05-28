f5d257ac325edd849f41437bc40fb41fa0dfef6d
Alec Arme & james williams <student@epicodus.com>
2015-05-28 10:19:53 -0700

    addd function testing up to 4

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe('count', function() {
   it('translates 2 to roman numerals', function (){
     expect(roman(2)).to.equal('II')
   });
+  it('translates 4 to roman numerals', function (){
+    expect(roman(4)).to.equal('IIII')
+  });
 });
