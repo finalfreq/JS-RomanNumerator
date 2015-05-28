992ddf30e02a1ddea8791e17501a2a8096137593
Alec Arme & james williams <student@epicodus.com>
2015-05-28 11:10:56 -0700

    Add front end. Add alert error for numbers over 4000.

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,89 @@
 var roman = function(number) {
   var output = []
-  var ones = []
-  var tens = []
-  var fifties = []
-  var hundreds = []
-  var fivehundreds = []
-  var thousands = []
-
+  if (number > 3999) {
+    alert("The romans didn't count past 3999!");
+  } else {
   while (number >= 1000) {
-    thousands.push("M")
+    output.push("M")
     number -= 1000;
   }
 
-
   while (number >= 900) {
-    fivehundreds.push("CM")
+    output.push("CM")
     number -= 900;
   }
 
   while (number >= 500) {
-    fivehundreds.push("D")
+    output.push("D")
     number -= 500;
-
   }
 
   while (number >= 400) {
-    hundreds.push("CD")
+    output.push("CD")
     number -= 400;
-
   }
 
   while (number >= 100) {
-    hundreds.push("C")
+    output.push("C")
     number -= 100 ;
   }
 
   while (number >= 90) {
-    fifties.push("XC")
+    output.push("XC")
     number -= 90 ;
   }
 
   while (number >= 50) {
-    fifties.push("L")
+    output.push("L")
     number -= 50 ;
   }
 
   while (number >= 40) {
-    tens.push("XL")
+    output.push("XL")
     number -= 40 ;
   }
 
   while (number >= 10) {
-    tens.push("X")
+    output.push("X")
     number -= 10 ;
   }
 
   while (number >= 9) {
-    ones.push("IX")
+    output.push("IX")
     number -= 9 ;
   }
 
   while (number >= 5) {
-    ones.push("V")
+    output.push("V")
     number -= 5 ;
   }
+
   while (number >= 4) {
-    ones.push('IV')
+    output.push('IV')
     number -= 4 ;
   }
+
   while (number >= 1) {
-    ones.push('I')
+    output.push('I')
     number -= 1 ;
   }
-  output.push(thousands.join(""), fivehundreds.join(""), hundreds.join(""), fifties.join(""), tens.join(""), ones.join(""))
+}
+
   output = output.join("");
   return output;
 };
+
+$(document).ready(function() {
+  $("form#calculator").submit(function(event) {
+    $("#result").show();
+    var number = $("input#number").val();
+    var result = roman(number);
+
+    $(".result").text(result);
+
+    event.preventDefault();
+
+  });
+
+
+});
