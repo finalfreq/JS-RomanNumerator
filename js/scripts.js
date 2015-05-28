46b35c0977bd51fba29f9bb856b77dd0425532cd
Alec Arme & james williams <student@epicodus.com>
2015-05-28 10:19:29 -0700

    addd function testing up to 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 var roman = function(number) {
-  return 'I';
+  var output = []
+
+  while (number >= 1) {
+    output.push('I')
+    number -= 1 ;
+  }
+
+  output = output.join("");
+  return output;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('count', function() {
   it('translates 1 to roman numerals', function (){
     expect(roman(1)).to.equal('I')
   });
+  it('translates 2 to roman numerals', function (){
+    expect(roman(2)).to.equal('II')
+  });
 });
